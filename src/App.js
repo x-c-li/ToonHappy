43758ffb2297a614e088b656ba3a469cd8fca476
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-12 18:39:43 -0500

    create preliminary nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EXAMPLE <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ src/components/Navigation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navigation.scss";
+
+function Navigation() {
+  return <div className="Navigation">TEST</div>;
+}
+
+export default Navigation;

2cd0d4d90d56236c3cb29cbce5e22147bc11f9a3
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-12 20:36:32 -0500

    create nav bar with img and links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Navigation.scss";
+import logo from "./imgs/logo.png";
 
 function Navigation() {
-  return <div className="Navigation">TEST</div>;
+  return (
+    <div className="Navigation">
+      <img src={logo} className="navbar-logo"></img>
+
+      <ul className="navbar-links">
+        <li>
+          <a href="">Stories</a>
+        </li>
+        <li>
+          <a href="">Toon Art </a>
+        </li>
+        <li>
+          <a href="">Read </a>
+        </li>
+      </ul>
+
+      <button className="navbar-login" type="submit">
+        Log In
+      </button>
+    </div>
+  );
 }
 
 export default Navigation;

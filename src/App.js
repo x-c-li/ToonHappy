51b6111e438a01aff8d87c1565caea52365c6133
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-12 20:50:22 -0500

    delete unnecessary content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           EXAMPLE <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Footer />
     </div>
   );
 }

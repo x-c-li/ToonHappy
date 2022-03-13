46e42badb4d5cb3160a9a38a63b2eb37fe75ad53
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-12 21:33:34 -0500

    create hero (non working button)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,13 +7,15 @@ function Hero() {
     <div className="Hero">
       <img src={logo} className="navbar-logo"></img>
 
-      <div className="hero">
-        <h2>Write.Connect.Create</h2>
+      <div className="hero-intro">
+        <h1>Write. Connect. Create</h1>
         <p>
           Our platform connects writers, toon artists, and audiences to create
           the webtoons we love.
         </p>
-        <button className="hero-sign-up">Sign Up Now!</button>
+        <button className="hero-sign-up">
+          <h2>Sign Up Now!</h2>
+        </button>
       </div>
     </div>
   );

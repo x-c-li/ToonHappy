486c875a309fef230dbd46df218af5c907ee1b9e
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-14 23:45:03 -0400

    add landing content and header styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,54 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import Hero from "./Hero";
-// import "./Landing.scss";
-// import logo from "./imgs/logo.png";
+import "./Landing.scss";
 
 function Landing() {
   return (
-    <div>
+    <div className="Landing">
       <Hero />
-      <div>HOW IT WORKS: ASDF ASDF ASDF ASDF ASDF ASDF ASDF</div>
+      <div>
+        <h1>How it works:</h1>
+
+        <div>
+          <Typography className="landing-header-light">
+            Step 1: Upload your work.
+          </Typography>
+          <p>Story/Script</p>
+          <p>Webtoon Art Sample</p>
+          <Typography>
+            Make your final edits to your story description and profile. Share
+            your work for community input or your profile.
+          </Typography>
+          <Typography>Hit Publish!</Typography>
+        </div>
+
+        <div>
+          <Typography className="landing-header-dark">For Writers</Typography>
+          <Typography>
+            Community members can vote on their favorite art and also leave beta
+            reader feedback on your story or script
+          </Typography>
+        </div>
+
+        <div>
+          <Typography className="landing-header-dark">For Artists</Typography>
+          <Typography>
+            Submit samples of your work to stories or upload samples to your
+            artist profile
+          </Typography>
+        </div>
+
+        <div>
+          <Typography className="landing-header-light">
+            Step 3: Connect!
+          </Typography>
+          <Typography>
+            Connect with your mutually chosen artist or writer and start
+            publishing episodes
+          </Typography>
+        </div>
+      </div>
     </div>
   );
 }

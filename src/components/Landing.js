a841309fae51f0eea36b0e2a4de5308e38078b07
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-15 00:17:38 -0400

    structure landing page steps

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 import Hero from "./Hero";
 import "./Landing.scss";
+import step3 from "./imgs/step-3-connect.png";
 
 function Landing() {
   return (
@@ -14,39 +15,54 @@ function Landing() {
           <Typography className="landing-header-light">
             Step 1: Upload your work.
           </Typography>
-          <p>Story/Script</p>
-          <p>Webtoon Art Sample</p>
-          <Typography>
-            Make your final edits to your story description and profile. Share
-            your work for community input or your profile.
-          </Typography>
-          <Typography>Hit Publish!</Typography>
+          <div className="landing-s1">
+            <p>Story/Script</p>
+            <p>Webtoon Art Sample</p>
+            <div className="landing-descr">
+              <Typography>
+                Make your final edits to your story description and profile.
+                Share your work for community input or your profile.
+              </Typography>
+              <Typography>Hit Publish!</Typography>
+            </div>
+          </div>
         </div>
 
         <div>
           <Typography className="landing-header-dark">For Writers</Typography>
-          <Typography>
-            Community members can vote on their favorite art and also leave beta
-            reader feedback on your story or script
-          </Typography>
+          <div className="landing-s1">
+            <span className="landing-s2">Step 2: Track Submissions</span>
+            <Typography className="landing-descr">
+              Community members can vote on their favorite art and also leave
+              beta reader feedback on your story or script
+            </Typography>
+          </div>
         </div>
 
         <div>
           <Typography className="landing-header-dark">For Artists</Typography>
-          <Typography>
-            Submit samples of your work to stories or upload samples to your
-            artist profile
-          </Typography>
+          <div className="landing-s1">
+            <Typography className="landing-s2">
+              Step 2: Submit to Stories
+            </Typography>
+            <Typography className="landing-descr">
+              Submit samples of your work to stories or upload samples to your
+              artist profile
+            </Typography>
+          </div>
         </div>
 
         <div>
           <Typography className="landing-header-light">
             Step 3: Connect!
           </Typography>
-          <Typography>
-            Connect with your mutually chosen artist or writer and start
-            publishing episodes
-          </Typography>
+          <div className="landing-s1">
+            <img src={step3} className="landing-step3"></img>
+            <Typography className="landing-descr">
+              Connect with your mutually chosen artist or writer and start
+              publishing episodes
+            </Typography>
+          </div>
         </div>
       </div>
     </div>

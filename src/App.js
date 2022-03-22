7a7adb75bad7d71c05aa7d7498a12f0dd90b4094
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-21 23:13:34 -0400

    add routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
-// import Footer from "./components/Footer";
 import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
+import Read from "./components/Read";
+import Stories from "./components/Stories";
+import ToonArt from "./components/ToonArt";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-
-      <Landing />
-
-      {/* <Footer /> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/read" element={<Read />} />
+          <Route path="/stories" element={<Stories />}></Route>
+          <Route path="/toonart" element={<ToonArt />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

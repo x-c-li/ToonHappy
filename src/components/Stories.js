241e4f64025efef4d645c82e242ff07d1bc6f67d
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-21 23:13:11 -0400

    create stories page

---
+++ src/components/Stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Stories() {
+  return (
+    <div className="Stories">
+      <div>This is the stories page.</div>
+    </div>
+  );
+}
+
+export default Stories;

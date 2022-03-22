0b0fb795bd17b55e72307f93018d569c519dc82b
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-21 23:13:01 -0400

    create toonart page

---
+++ src/components/ToonArt.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToonArt() {
+  return (
+    <div className="ToonArt">
+      <div>This is the Toon Art page.</div>
+    </div>
+  );
+}
+
+export default ToonArt;

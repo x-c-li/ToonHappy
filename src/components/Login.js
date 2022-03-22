021c3e1ee41c4c3d43ee7667d48586fc6a3d6baf
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-21 23:12:10 -0400

    create login page

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div>
+        asdfasfdja;lsdkjf;lsakfa;lsjfdlaskjfd;lsakjfd;lsakjfda;lskjfdlsakjfd;lsakjfdalsdkjflk
+      </div>
+    </div>
+  );
+}
+
+export default Login;

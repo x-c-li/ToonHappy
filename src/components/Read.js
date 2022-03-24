0062723029283f68a62864dd78b2d3c851c628cd
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-23 22:09:25 -0400

    add buttons to make into filters

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+//import fake data from a js file
 function Read() {
   return (
     <div className="Read">
-      <div>Find a story</div>
+      <h1>Find a story</h1>
+
+      <div className="menu-tabs container">
+        <div className="menu-tab d-flex justify-content-around"></div>
+        <button className=" btn btn-warning">Just Stories</button>
+        <button className=" btn btn-warning">Just Art</button>
+        <button className=" btn btn-warning">All</button>
+        <button className=" btn btn-warning">Top Rated</button>
+        <text>Genre</text>
+      </div>
+
+      <div>Stories Art</div>
       <div> Drama Action Adventure Romance Slice of Life Comedy</div>
       <div>Stories Stories Stories Stories</div>
     </div>

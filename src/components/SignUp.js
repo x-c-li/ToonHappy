0cf1c1d1da512a069af93584edc707e52c1f369e
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-26 12:26:07 -0400

    fixed spacing

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,46 +9,42 @@ function SignUp() {
         Join a community of fans, writers and artists to create the best stories
         ever.
       </div>
-      <div className="form-field">
-        <div className="form-group">
-          <label>First Name</label>
+      <div>
+        <form className="form-field">
+          <label for="first-name">First Name</label>
           <input
             type="text"
             name="first-name"
             placeholder="Please enter your first name"
           />
-        </div>
-        <div className="form-group">
+
           <label>Last Name</label>
           <input
             type="text"
             name="last-name"
             placeholder="Please enter your last name"
           />
-        </div>
-        <div className="form-group">
+
           <label>Email</label>
           <input
             type="text"
             name="email"
             placeholder="Please enter a valid email"
           />
-        </div>
-        <div className="form-group">
+
           <label>Username</label>
           <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div className="form-group">
+
           <label>Password</label>
           <input type="text" name="password" placeholder="Password" />
-        </div>
-        <div className="form-group">
+
           <label>Confirm Password</label>
           <input type="text" name="password" placeholder="Confirm Password" />
-        </div>
-        <div>
-          <button className="signup-button">Sign Up!</button>
-        </div>
+
+          <div>
+            <button className="signup-button">Sign Up!</button>
+          </div>
+        </form>
       </div>
     </div>
   );

03f95cd10ddb6a02d0568643b1620931b161b8e4
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-03-29 21:56:07 -0400

    reformat login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,25 @@
 import React from "react";
 import logo from "./imgs/logo.png";
+import "./Login.scss";
 
 function Login() {
   return (
-    <div>
+    <div className="Login">
       <div>
-        <img src={logo}></img>
+        <img className="login-img" src={logo}></img>
       </div>
-      <div className="form-group">
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username" />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input type="text" name="password" placeholder="Password" />
-      </div>
-      <div>
-        <button>Login</button>
+      <div className="form-field">
+        <form>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Username" />
+
+          <label>Password</label>
+          <input type="text" name="password" placeholder="Password" />
+
+          <div>
+            <button className="signup-button">Login</button>
+          </div>
+        </form>
       </div>
     </div>
   );

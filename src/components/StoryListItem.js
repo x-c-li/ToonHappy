f999331240b4cd2893551aecf3000fa602b55f62
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-04-27 23:17:51 -0400

    add styling to example of story

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -1,4 +1,7 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import "./StoryListItem.scss";
+
 //import fake data from a js file
 
 import story1 from "./imgs/story-ex-1.jpeg";
@@ -6,11 +9,9 @@ import story1 from "./imgs/story-ex-1.jpeg";
 function StoryListItem() {
   return (
     <div className="StoryListItem">
-      <div>image title</div>
-      <div>image1 title1</div>
       <div>
-        image2 title2
         <img src={story1} className="story-cover"></img>
+        <Typography>Story 1 Title</Typography>
       </div>
     </div>
   );

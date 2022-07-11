dc8dd83f059eb889e719c4bd7201f5e6c5544559
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-10 20:18:32 -0400

    clean up code

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -9,19 +9,6 @@ function StoryListItem(props) {
         <img src={props.image} className="story-cover"></img>
         <Typography>{props.title}</Typography>
       </div>
-      {/* <div>
-        <img src={noArt} className="story-cover"></img>
-        <Typography>The Stars Between Us</Typography>
-      </div>
-      <img src={story2} className="story-cover"></img>
-
-      <img src={story3} className="story-cover"></img>
-
-      <img src={story4} className="story-cover"></img>
-
-      <img src={story7} className="story-cover"></img>
-
-      <img src={story8} className="story-cover"></img> */}
     </div>
   );
 }

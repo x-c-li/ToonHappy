f2040d94c857420484155e7ed9c6e303702ca17a
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-10 20:15:42 -0400

    created fake data in storylist and passed to storylistitem

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,12 +1,77 @@
 import React from "react";
 import StoryListItem from "./StoryListItem";
-//import fake data from a js file
+
+import noArt from "./imgs/no_art.png";
+import story1 from "./imgs/story-ex-1.jpeg";
+import story2 from "./imgs/story-ex-2.jpeg";
+import story3 from "./imgs/story-ex-3.jpeg";
+import story4 from "./imgs/story-ex-4.jpeg";
+import story5 from "./imgs/story-ex-5.jpeg";
+import story6 from "./imgs/story-ex-6.jpeg";
+import story7 from "./imgs/story-ex-7.jpeg";
+import story8 from "./imgs/story-ex-8.jpeg";
+
+//fake data
+
 function StoryList() {
-  return (
-    <div className="StoryList">
-      <StoryListItem />
-    </div>
-  );
+  const stories = [
+    {
+      image: story1,
+      title: "Gaming Love",
+    },
+    {
+      image: noArt,
+      title: "The Stars Between Us",
+    },
+    {
+      image: story2,
+      title: "I'm the Asshole",
+    },
+    {
+      image: story3,
+      title: "Call From The Beyond",
+    },
+    {
+      image: story4,
+      title: "At The Rising Sun",
+    },
+    {
+      image: noArt,
+      title: "Arranging My Funeral",
+    },
+    {
+      image: story5,
+      title: "My Perfect Heartbreak",
+    },
+    {
+      image: story6,
+      title: "Travelling In Your Memory",
+    },
+    {
+      image: noArt,
+      title: "Winning The Game",
+    },
+    {
+      image: story7,
+      title: "Coming Back From You",
+    },
+    {
+      image: story8,
+      title: "Fight Depression As A Furry",
+    },
+    {
+      image: noArt,
+      title: "My Worst Enemy Confessed Their Love",
+    },
+  ];
+  const item = stories.map((item) => {
+    return (
+      <div className="StoryList">
+        <StoryListItem image={item.image} title={item.title} />
+      </div>
+    );
+  });
+  return <div>{item}</div>;
 }
 
 export default StoryList;
--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -2,17 +2,26 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 import "./StoryListItem.scss";
 
-//import fake data from a js file
-
-import story1 from "./imgs/story-ex-1.jpeg";
-
-function StoryListItem() {
+function StoryListItem(props) {
   return (
     <div className="StoryListItem">
       <div>
-        <img src={story1} className="story-cover"></img>
-        <Typography>Story 1 Title</Typography>
+        <img src={props.image} className="story-cover"></img>
+        <Typography>{props.title}</Typography>
       </div>
+      {/* <div>
+        <img src={noArt} className="story-cover"></img>
+        <Typography>The Stars Between Us</Typography>
+      </div>
+      <img src={story2} className="story-cover"></img>
+
+      <img src={story3} className="story-cover"></img>
+
+      <img src={story4} className="story-cover"></img>
+
+      <img src={story7} className="story-cover"></img>
+
+      <img src={story8} className="story-cover"></img> */}
     </div>
   );
 }
